1e6155aaa1b7397e98716d68d94bb0a38a02f387
Firestarme <firestarme@gmail.com>
2014-11-17 16:58:26 +0000

    Added original computercraft files

---
+++ cc/Account.lua
@@ -0,0 +1,309 @@
+-- ## Access Structure ##--
+-- *defined by recieved table
+
+--* n : account number to be accessed
+--* f : same as n for transactions (account the money is transferred from)
+--* t : same as n for transactions (account the money is transferred to)
+-- na : the account info (loaded by the api)
+--		-name : the name of the account holder
+--		-amount : the current balance
+--		-ohpin : the original hashed pin 
+--		-ohtcode : the original transac code (pin alternative for transactions)
+--		-frozen : whether the account has been frozen
+-- fa : same as na for transactions (account the money is transferred from)
+-- ta :	same as na for transactions (account the money is transferred to)
+-- *hpin : the hashed pin supplied by the request
+-- *htcode: the hashed tcode supplied by the request
+-- *op : the operation that is requested (cash,transac,bal,nPin,nTcode,freeze)
+-- *name : the name supplied by the request (used for creating an account)
+-- *nhpin : new hashed pin (used for changing your pin)
+-- *nhtcode : new hashed tcode (used for changing your tcode)
+
+--## local utility functions ##--
+
+local opt = {cash,transac,bal,nPin,nTcode,freeze}
+	
+local function readFile(path)
+
+		h = fs.open(path,"r")
+		str = h.readAll()
+		h.close()
+		return str
+
+end
+
+local function saveFile(str,path)
+
+	h = fs.open(path,"w")
+	h.write(str)
+	h.close()
+
+end	
+
+local function log(lpath,l)
+
+	if fs.exists(lpath) then
+	
+		h = fs.open(lpath,"a")
+		h.writeLine(l)
+		h.close()
+		
+	else
+	
+		h = fs.open(lpath,"w")
+		h.writeLine(l)
+		h.close()
+		
+	end
+	
+end
+
+local function genOut(num)
+	
+	if num > 0 then 
+	
+		a = "+" 
+		
+	else 
+	
+		a = "-" 
+		
+	end
+	
+	return a..tostring(math.abs(num))
+
+end
+
+local function vPin(ohpin,hpin)
+
+	assert(ohpin == hpin,"Pin Incorrect")
+
+end
+
+local function vTcode(ohpin,hpin,ohtcode,htcode)
+
+	assert(ohpin == hpin or ohtcode == htcode,"Pin or transac code Incorrect")
+
+end
+
+local function vfrozen(frozen)
+
+	assert(not frozen,"Your account has been frozen")
+
+end
+
+local function nameCheck(name,n,path)
+
+	if fs.exists(path) then
+		
+		nList = textutils.unserialize(readFile(path))
+		
+		for k,v in pairs(nList) do 
+			
+			assert(name ~= v,"You already have an account")
+			
+		end
+		
+	else
+			
+			nList = {}
+			
+	end
+	
+	nList[n] = name
+	
+	saveFile(textutils.serialize(nList))
+
+end
+
+local function nilCheck(var,varname)
+
+	assert(var ~= nil,"no "..varname.." specified")
+
+end
+
+local function getOpt()
+
+	return opt
+
+end
+	
+--## Account Operation Sub-API ##--
+
+function opt.cash()
+	setfenv(1,opt.access)
+
+	vPin(na.ohpin,hpin)
+	vfrozen(na.frozen)
+	
+	nilCheck(q,"Quantity")
+	
+	local b = na.amount + q
+	assert(b >= 0,"Funds are unavailable")
+	na.amount = b
+		
+	log(lRoot..n,genOut(q).." :Cash")
+
+end
+
+
+function opt.transac()
+	setfenv(1,opt.access)
+
+	vTcode(fa.ohpin,hpin,fa.ohtcode,htcode)
+	vFrozen(fa.frozen)
+	
+	nilCheck(q,"Quantity")
+	
+	q = math.abs(q)
+	
+	local b = fa.amount - q
+	assert(b >= 0,"Funds are unavailable")
+	fa.amount = b
+		
+	log(lRoot..f,genOut(-q).." :Transfered To "..t.." ("..ta.name..")")
+	
+	ta.amount = ta.amount + q
+
+	log(lRoot..t,genOut(q).." :Transfered From "..f.." ("..fa.name..")")
+end
+
+function opt.bal()
+	setfenv(1,opt.access)
+
+	return na.amount
+	
+end
+
+function opt.nPin()
+
+	vPin(na.ohpin,hpin)
+	
+	nilCheck(nhpin,"new pin")
+	
+	na.ohpin = nhpin
+
+end
+
+function opt.nTcode()
+
+	vPin(na.ohpin,hpin)
+	
+	nilCheck(nhtcode,"new transac code")
+	
+	na.ohtcode = nhtcode
+
+end
+
+function opt.freeze()
+
+	vPin(na.ohpin,hpin)
+	
+	na.frozen = true
+
+end
+	
+--## Account Access API ##--
+		
+access = {
+
+	aRoot = "data/accounts/",
+	lRoot = "data/logs/",
+	nxNumPath = "data/nxNum",
+	nListPath = "data/accounts/nList",
+	tonumber = tonumber,
+	assert = assert,
+ 	print = print,
+	setfenv = setfenv,
+	math = math,
+	textutils = textutils,
+	fs = fs
+	
+}	
+
+function access:new(o)
+	o = o or {}
+	setmetatable(o,self)
+	self.__index = self
+	return o
+end
+
+function access:open()
+	setfenv(1,self)
+	
+	if op == "transac" then
+		
+		nilCheck(f,"account number (from)")
+		nilCheck(t,"account number (to)")
+		
+		fpath = aRoot..f
+		tpath = aRoot..t
+		
+		assert(fs.exists(fpath),"account "..f.."Does not exist")
+		assert(fs.exists(tpath),"account "..t.."Does not exist")
+		
+		fa = textutils.unserialize(readFile(aRoot..f))
+		ta = textutils.unserialize(readFile(aRoot..t))
+	else
+		
+		nilCheck(n,"account number")
+		
+		path = aRoot..n
+		
+		assert(fs.exists(path),"account "..n.."Does not exist")
+
+		na = textutils.unserialize(readFile(aRoot..n))
+	end
+end
+
+function access:close()
+	setfenv(1,self)
+	
+	if op == "transac" then
+		
+		saveFile(textutils.serialize(fa),fpath)
+		saveFile(textutils.serialize(ta),tpath)
+	
+	else
+	
+		saveFile(textutils.serialize(na),path)
+	
+	end	
+end
+
+function access:operate()
+	setfenv(1,self)
+	
+  	local opt = getOpt()
+  
+	opt.access = self
+  	res = opt[op]()
+	return res	
+	
+end
+
+function access:create()
+	setfenv(1,self)
+	
+	nilCheck(name,"name")
+	nilCheck(hpin,"pin")
+	nilCheck(htcode,"transac code")
+	
+	na = {
+		
+		amount = 150,	
+		name = name,
+		ohpin = hpin,
+		ohtcode = htcode,
+		frozen = false
+		
+	}
+	
+	n = tonumber(readFile(nxNumPath))
+	saveFile(n+1,nxNumPath)
+	
+	checkName(name,n,nListPath)
+	
+	path = aRoot..n
+end
+
---
+++ cc/SNC.lua
@@ -0,0 +1,174 @@
+--## sets the default options ##--
+
+StConduit =	{
+	id = 0,
+	psk = "Default",
+	pr = "dftStCond",
+	hn = "dftHost",
+	ka = "RSA",
+	ks = 526,
+	ma = "RSA",
+	ha = "SHA256",
+	to = 50,
+	suc = false,
+	s = "back"
+}
+
+--## variables used ##--
+-- id: target id
+-- idn: recieved target id
+-- psk: pre shared key (for HMAC)
+-- cr: wrapped cryptographic accelerater
+-- pr: protocol
+-- hs: hostname
+-- ka: key algorithim
+-- ha: hashing algorithim
+-- ma: message algorithim
+-- ks: key size
+-- to: timeout
+-- suc: id  match sucess
+-- s: side of modem
+
+--## adds standard apis to the table ##--
+
+	StConduit.math = math
+	StConduit.pairs = pairs
+	StConduit.print = print
+	StConduit.setfenv = setfenv
+	StConduit.textutils = textutils
+	StConduit.fs = fs
+	StConduit.string = string
+	StConduit.rednet = rednet
+
+
+--## Local functions called by the interface ##--
+
+local function loadPSK(id)
+	h = fs.open("data/crypt/"..id,"r")
+	str = h.readAll() 
+	h.close()
+	return str
+end
+
+local function deconcat(sy,str)
+	a = string.match(str,"[^"..sy.."]+")
+	b = string.sub(string.match(str,sy.."[^"..sy.."]+"),2)
+	return a,b
+end
+
+local function generatehmac(str,psk,cr,ha)
+	return cr.hash(ha,psk..cr.hash(ha,psk..str))
+end
+
+local function authhmac(hmac,str,psk,cr,ha)
+	assert(hmac == generatehmac(str,psk,cr,ha),"Failed to Authenticate")
+end
+
+local function prepMsg(str,psk,cr,ha)
+	return str.."$"..generatehmac(str,psk,cr,ha)
+end
+
+local function sendMsg(id,msg,pr,cr,ha)
+	psk = loadPSK(id)
+	rednet.send(id,prepMsg(msg,psk,cr,ha),pr)
+end
+
+
+local function receiveMsg(id,pr,cr,ha,to)
+	local t = os.clock()
+	local idn
+	
+	if id ~= nil then
+		while idn ~= id do 
+			idn,msg = rednet.receive(pr,to)
+			if to ~= nil then
+				if os.clock() >= t+to then error("Request Timed Out") end
+			end
+		end
+	else
+		idn,msg = rednet.receive(pr,to)
+	end
+	
+	suc = pcall(function() psk = loadPSK(idn) end) 
+	
+	if suc then
+		str,hmac = deconcat("$",msg)
+		authhmac(hmac,str,psk,cr,ha)
+	end
+		return str,idn,suc
+end
+
+local function sendKey(id,pr,cr,ka,ks,cr,ha)
+	pub,pri = cr.generateKeyPair(ka,ks)
+	sendMsg(id,pub.encode(),pr,cr,ha)
+	return pri
+end
+
+local function receiveKey(id,pr,ka,cr,ha,to)
+	str,idn,suc = receiveMsg(id,pr,cr,ha,to)
+	pub = cr.decodeKey(ka,str)
+	return pub,idn,suc
+end
+
+local function sendMsgEnc(id,msg,pr,cr,ha,pri,ma)
+	comm = pri.encrypt(ma,msg)
+	sendMsg(id,comm,pr,cr,ha)
+end
+
+local function receiveMsgEnc(id,pr,cr,ha,pub,ma,to)
+	comm = receiveMsg(id,pr,cr,ha,to)
+	return pub.decrypt(ma,comm)
+end
+
+--## main API  ##--
+
+function StConduit:newConduit(o,pr,hn,cr,to)
+	o = o or {}
+	o.pr = pr or o.pr
+	o.hn = hn or o.hn
+	o.cr = cr or o.cr
+	o.to = to or o.to
+	setmetatable(o,self)
+	self.__index = self
+	return o
+end
+
+function StConduit:init()
+	setfenv(1,self)
+	
+	rednet.open(s)
+	
+	id = rednet.lookup(pr,hn)
+	pri = sendKey(id,pr..":k",cr,ka,ks,cr,ha)
+	pub = receiveKey(id,pr..":k",ka,cr,ha,to)
+	
+end
+
+function StConduit:serverInit()
+	setfenv(1,self)
+
+	rednet.open(s)
+	
+	rednet.host(pr,hn)
+	
+	while not suc do
+		pub,id,suc = receiveKey(nil,pr..":k",ka,cr,ha)
+	end
+	
+	pri = sendKey(id,pr..":k",cr,ka,ks,cr,ha)
+	
+end
+
+function StConduit:send(msg)
+	setfenv(1,self)
+
+	sendMsgEnc(id,msg,pr,cr,ha,pri,ma)
+	
+end
+
+function StConduit:receive(to)
+	self.to = to or self.to
+	setfenv(1,self)
+
+	return receiveMsgEnc(id,pr,cr,ha,pub,ma,to)
+end

dd6c6fb229fc023fb18a7d7fb93a0742baacc79f
Firestarme <firestarme@gmail.com>
2014-11-17 17:21:06 +0000

    corrected server file

---
+++ cc/BankServer.lua
@@ -0,0 +1,61 @@
+--## load APIS ##--
+
+os.loadAPI("SNC")
+os.loadAPI("AMS")
+
+--## Defining server functions and variables ##--
+
+args = {
+
+	cr = peripheral.find("cryptographic accelerator"),
+	pr = "BankNet",
+	hn = "BankServer"
+
+}
+
+MainCond = SNC.StConduit:newConduit(args)
+
+function receive(cond)
+	
+	rec = cond:receive()
+  	print(rec)
+	return textutils.unserialize(rec)
+	
+end
+
+function operate(o)
+	
+	acc = AMS.access:new(o)
+	acc:open()
+	ret = acc:operate()
+	acc:close()
+	
+	return ret
+	
+end
+
+
+
+--## Main Server Functionality ##--
+
+while true do
+
+	cond = MainCond:newConduit()
+	cond:serverInit()
+	
+	while true do
+		condSuc,o = pcall(receive,cond)
+		
+		if condSuc then
+			
+			err,ret = pcall(operate,o)
+			
+			cond:send(textutils.serialize({err = err,ret = ret}))
+			
+		else print(o)
+	  	end
+		
+		if o.close then break end
+		
+	end
+end
